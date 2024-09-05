27de1885d35b9d1a6009a0a1be23a81bfd7c0406
CJW <153478814+JeongWoo9906626@users.noreply.github.com>
2024-09-05 17:28:16 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(string myString) 
+{
+	std::string answer = myString;
+	std::transform(answer.begin(), answer.end(), answer.begin(), ::tolower);
+	return answer;
+}
