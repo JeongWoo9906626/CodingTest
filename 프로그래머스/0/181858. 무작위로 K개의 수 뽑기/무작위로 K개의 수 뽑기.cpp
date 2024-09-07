dc3f1896fddacfde5782c39455d06527ae1c0f98
CJW <153478814+JeongWoo9906626@users.noreply.github.com>
2024-09-07 23:41:30 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 1.00 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.cpp
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.cpp
@@ -8,28 +8,20 @@ using namespace std;
 vector<int> solution(vector<int> arr, int k)
 {
 	std::vector<int> answer;
-	std::vector<int> IndexVector;
-	int Temp = k;
+	std::set<int> Set;
 	
-	for (int i = 0; i < arr.size(); ++i)
+	for (int Value : arr)
 	{
-		if (IndexVector.end() == std::find(IndexVector.begin(), IndexVector.end(), arr[i]))
+		if (0 == Set.count(Value) && answer.size() < k)
 		{
-			IndexVector.emplace_back(arr[i]);
+			Set.insert(Value);
+			answer.emplace_back(Value);
 		}
 	}
-
-	int CheckNum = k > IndexVector.size() ? IndexVector.size() : k;
-
-	for (int i = 0; i < CheckNum; ++i)
-	{
-		answer.emplace_back(IndexVector[i]);
-	}
-
-	for (int i = 0; i < k - CheckNum; ++i)
+	int CheckNumber = answer.size();
+	for (int i = 0; i < k - CheckNumber; ++i)
 	{
 		answer.emplace_back(-1);
 	}
-
 	return answer;
 }
