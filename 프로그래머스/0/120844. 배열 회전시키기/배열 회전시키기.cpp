2f98534d2e1c21322b869884e4b0a7f7f8b7d935
CJW <153478814+JeongWoo9906626@users.noreply.github.com>
2024-10-03 17:39:11 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.cpp
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.cpp
@@ -1,40 +1,19 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> numbers, string direction) {
     vector<int> answer;
-    
-    if (direction == "right")
+    if(direction =="left")
     {
-        int End = numbers[numbers.size() - 1];
-        int Next = numbers[0];
-        int NextNext = 0;
-        for (int i = 0; i < numbers.size() - 1; ++i)
-        {
-            NextNext = numbers[i + 1];
-            numbers[i + 1] = Next;
-            Next = NextNext;
-        }
-        numbers[0] = End;
-        answer = numbers;
+       std::rotate(numbers.begin(),numbers.begin() + 1,numbers.end());
     }
     else
     {
-        int First = numbers[0];
-        int Next = numbers[numbers.size() - 1];
-        int NextNext = 0;
-        for (int i = numbers.size() - 1; i > 0; --i)
-        {
-            NextNext = numbers[i - 1];
-            numbers[i - 1] = Next;
-            Next = NextNext;
-        }
-        numbers[numbers.size() - 1] = First;
-        answer = numbers;
+        std::rotate(numbers.begin(),numbers.end() - 1,numbers.end());
     }
-    
-    
+    answer = numbers;
     return answer;
 }
