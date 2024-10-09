ef0060b784bf8e247247307d3773fb33de77b603
CJW <153478814+JeongWoo9906626@users.noreply.github.com>
2024-10-09 17:49:57 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, vector<int> numlist) {
+    vector<int> answer;
+    
+    for (int i = 0; i < numlist.size(); ++i)
+    {
+        if (0 == numlist[i] % n)
+        {
+            answer.emplace_back(numlist[i]);
+        }
+    }
+    
+    return answer;
+}
