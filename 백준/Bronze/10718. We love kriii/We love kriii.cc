c87ebcf42f4fe3af367a74f12fa73b188d58f439
CJW <153478814+JeongWoo9906626@users.noreply.github.com>
2024-11-09 23:16:18 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout << "강한친구 대한육군" << '\n';
+    std::cout << "강한친구 대한육군" << '\n';
+    return 0;
+}
